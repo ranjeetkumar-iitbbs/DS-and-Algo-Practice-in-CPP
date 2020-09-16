871afbb5ce652ee346e96122ea136eb288f71c9b
RANJEET KUMAR <71075284+ranjeetkumar-iitbbs@users.noreply.github.com>
2020-09-16 16:35:19 +0530

    Round Robin Elimination Using C++
    Round-Robin Elimination Problem: There are n people standing in a circle. In the first step, the counting starts at person 1 and proceeds around the circle, and the k-th person
    is eliminated. 
    Next, the counting starts from the person next to the eliminated one, then k-1 people are skipped and the k-th person is eliminated. The elimination proceeds around the circle until the last person remains, who is the winner. 
    
    Given n people and a number k,
    write a CPP  program that outputs the winner.
    
    Example 1: If n = 5 and k = 2, then the winner is person 3.
    First, the person at position 2 is eliminated, then person at position 4 is eliminated, then person at position 1 is eliminated. Finally, the person at position 5 is eliminated. So the person at position 3 wins.
    
    Example 2: If n = 7 and k = 3, then the winner is person 4.
    First, the person at position 3 is eliminated, then person at position 6 is eliminated, then person at position 2 is eliminated, then person at position 2 is eliminated, then person at position 7 is eliminated. Finally, the person at position 1 is eliminated. So the person at position 4 wins.

---
+++ A2.1_20CS06011.cpp
@@ -0,0 +1,106 @@
+/*@	Ranjeet Kumar - 20CS06011
+	Assignment : 2
+	Round Robin Elimination	
+*/
+
+#include<iostream>
+using namespace std;
+
+class node{
+	public:
+		int data;
+		node *next;
+		node(int data){
+			this -> data = data;
+			this -> next = NULL;
+		}
+};
+
+class queue{
+	private:
+		node* front;
+		node* rear;
+		int size;
+		
+	public:
+		queue(){
+			front = NULL;
+			rear = NULL;
+			size = 0;
+		}
+		
+		void enqueue(int data, int n){
+			node *newNode = new node(data);
+			
+			if(data == 1){
+				front = newNode;
+				rear = newNode;
+				size++;
+			}
+			else{
+				if(data == n){
+					rear -> next = newNode;
+					rear = newNode;
+					rear -> next = front;
+					size++;
+				}
+				else{
+					rear -> next = newNode;
+					rear = newNode;
+					size++;
+				}
+			}
+			
+		}
+		
+		int print(){
+			return front -> data;
+		}
+		
+		int getSize(){
+			return size;
+		}
+		void dequeue(int index){
+			node* temp;
+			int i = 1;
+			while(i < index){
+				temp = front;
+				front = front -> next;
+				i++;
+			}
+			
+			temp -> next = front -> next;
+			delete front;
+			front = temp -> next;
+			size--;
+		}
+};
+
+int main(){
+	int n, k;
+	cin >> n;
+	cin >> k;
+	
+	queue q;
+	
+	for(int i = 1; i <= n; i++){
+		q.enqueue(i, n);
+	}
+	
+	if(n <= 0 || k <= 0){
+		cout << "Please enter value of N and K greater than or equal to 1";
+		return 0;
+	}
+	
+	if(n == 1){
+		cout << "Winner is :: 1";
+		return 0;
+	}
+	else{
+		while(q.getSize() != 1){
+			q.dequeue(k);
+		}
+		cout << "Winner is :: " << q.print();
+	}
+	
+}
